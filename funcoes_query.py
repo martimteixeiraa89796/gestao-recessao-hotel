182762b057d611b459d7d90eecb1f3ed2fd2f3b6
Martim Teixeira <a89796@ualg.pt>
2025-11-01 18:37:02 +0000

    Criado ficheiro funcoes_query.py para alojar SQL e exemplo de formato

---
+++ funcoes_query.py
@@ -0,0 +1,21 @@
+#Este ficheiro é onde vai ser colocado funções com comandos de SQL
+#O objetivo é que estas sejam chamadas dentro de um ficheiro python
+#onde depois a classe que está dentro de ferramentas_BD.py
+#vai depois ser iniciada e utilizada.
+
+#Isto é somente para separar a query e a lógica do código.
+
+
+
+#Exemplo de função e estrutura
+def função(exemplo, exemplo2, exemplo3):  #<--- Colocar argumentos para alterar partes da query antes de ser mandada para ser executada
+    """Função de exemplo"""
+    
+    #Query é construída com as variáveis de cima
+    query = f"""
+        SELECT * FROM {exemplo};
+    """
+
+    return query #Retorna a query finalizada para depois ser executada
+    #A query seria guardada numa variável para depois ser executada noutro lado
+    
