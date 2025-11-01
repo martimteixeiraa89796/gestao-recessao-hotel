93495d7ba8b7f60283f4678e5b5690bf7f93e5ab
Martim Teixeira <a89796@ualg.pt>
2025-11-01 11:23:40 +0000

    Adicionada lógica para criação de base de dados

---
+++ criarBD.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+sqliteConnection = sqlite3.connect('basedados.db')
+cursor = sqliteConnection.cursor()
+
+query = """
+    Escrever SQL para criação da base de dados aqui
+"""
+cursor.execute(query)
+cursor.close()
