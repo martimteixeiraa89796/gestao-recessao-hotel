4547a1835197a0c396a3ae36fd2ee992f96dff71
Martim Teixeira <a89796@ualg.pt>
2025-11-02 19:30:40 +0000

    Criado loop da aplicação com código de teste

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 #Importar livrarias criadas para o projeto
 import ferramentas_BD  #Interagir com BD
 import funcoes_query  #SQL para ser usado na BD
+
+
+#Loop da aplicação
+while True:
+    input("Prima ENTER para testar aplicação: ")
+    print("Hello World")
