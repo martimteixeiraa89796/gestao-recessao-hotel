b6c57a777f5a35e226e23a6b0be0ff0734eb1cde
Martim Teixeira <a89796@ualg.pt>
2025-11-02 19:31:56 +0000

    Adicionado comentário relacionado a verificação inicial de BD

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ import funcoes_query  #SQL para ser usado na BD
 while True:
     input("Prima ENTER para testar aplicação: ")
     print("Hello World")
+
+    #O programa deve primeiro verificar se a base de dados existe e contém o que é necessário para funcionar.
+    #O programa deve verificar se as tabelas exixtem.
+    #Caso não existam, o programa deve preparar e criar a base de dados.
