117c82a09fdabbd5bef364f6205790291a8a808d
Martim Teixeira <a89796@ualg.pt>
2025-11-20 17:11:55 +0000

    Movido funções relacionadas com escolhas para ficheiro separado

--- ferramentas_BD.py
+++ ferramentas_BD.py
@@ -166,26 +166,3 @@ def get_tabelas():
     tabelas.remove("sqlite_sequence")
 
     return tabelas
-
-
-def listar_escolhas(lista):
-    print("Opções disponíveis:")
-    for item in range(len(lista)):
-        print(f"    {item + 1}. {lista[item]}".title())
-
-
-def fazer_escolha(lista):
-    while True:
-        try:
-            escolha = int(input("Escolha uma opção: "))
-
-            if 0 < escolha <= len(lista):
-                break
-
-            else:
-                print("Opção não disponivel")
-        
-        except ValueError:
-            pass
-    
-    return lista[escolha-1]
