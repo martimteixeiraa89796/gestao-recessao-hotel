7420bd2d8ecdeeb369249830d9302f0d8c2f1ee0
Martim Teixeira <a89796@ualg.pt>
2025-11-20 20:24:45 +0000

    Adicionado função para escolha de tipo de dado sql

--- ferramentas_escolha.py
+++ ferramentas_escolha.py
@@ -19,3 +19,45 @@ def fazer_escolha(lista):
             pass
     
     return lista[escolha-1]
+
+
+def sql_escolher_tipo():
+    while True:
+        while True:
+            resposta = input("--> ")
+
+            if not resposta:
+                pass
+
+            else:
+                break
+
+        tipo_dado_lista = ["sql int",
+                        "sql decimal",
+                        "sql string"]
+
+        print("Escolha formato.")
+        listar_escolhas(tipo_dado_lista)
+
+
+        escolha = fazer_escolha(tipo_dado_lista)
+
+        try:
+            if escolha == tipo_dado_lista[0]:
+                resposta = int(resposta)
+            
+            elif escolha == tipo_dado_lista[1]:
+                resposta = float(resposta)
+            
+            elif not escolha:
+                continue
+            
+            else:
+                pass
+            
+            break
+
+        except ValueError:
+                print("Formato incompatível.")
+
+    return resposta
