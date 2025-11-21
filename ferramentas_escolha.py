0aa1169d57d4f497392bab26677ab9239a837778
Martim Teixeira <a89796@ualg.pt>
2025-11-21 19:43:19 +0000

    Atualizada documentação em ferramentas_escolha.py

--- ferramentas_escolha.py
+++ ferramentas_escolha.py
@@ -1,10 +1,45 @@
+"""
+Módulo com função para ajudar com input do utilizador
+-----------------------------------------------------
+
+Este módulo contém funções com tudo o que é necessário para se interagir com o utilizador.
+"""
+
 def listar_escolhas(lista):
+    """
+    Imprime uma listagem de listas.
+
+    Esta função faz uma listagem numérica do que lhe é fornecido.
+    Isto é feito através da iteração na lista e construção de strings para serem imprimidas.
+
+    :param lista: Lista com os objetos para listagem
+    :type lista: list
+
+    Exemplo de listagem:
+
+    >>> listar_escolhas(["Azul", "Vermelho", "Amarelo"])
+    """
+
     print("Opções disponíveis:")
     for item in range(len(lista)):
         print(f"    {item + 1}. {lista[item]}".title())
 
 
 def fazer_escolha(lista):
+    """
+    De uma lista, pregunta *input* ao utilizador e retorna a resposta.
+
+    Esta função cria um loop onde o utilizador tem de escolher uma opção baseado numa lista que
+    é fornecida á função. O loop termina se a escolha for válida.
+
+    :return: Opção escolhida pelo utilizador
+    :rtype: Depende do valor inseridos
+
+    Exemplo de ambiente de escolha:
+
+    >>> fazer_escolha(["Banana", "Maçã", "Pera"])
+    """
+
     while True:
         try:
             escolha = int(input("Escolha uma opção: "))
@@ -22,6 +57,20 @@ def fazer_escolha(lista):
 
 
 def sql_escolher_tipo():
+    """
+    Pede ao utilizador para introduzir dados e formato sql equivalente.
+
+    Esta função pede ao utilizador para inserir dados e o formato que o mesmo pretende.
+    A função faz depois a tradução entre os tipos em Python que são equivalente no sql.
+
+    :return: Resposta do utilizador traduzida
+    :rtype: string, int ou float
+
+    Exemplo de execução:
+
+    >>> sql_escolher_tipo()
+    """
+
     while True:
         while True:
             resposta = input("--> ")
