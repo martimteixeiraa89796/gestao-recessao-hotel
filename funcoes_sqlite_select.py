62cdd5c5e927741ced3a364f15798b14257ef48a
Martim Teixeira <a89796@ualg.pt>
2025-11-21 14:19:51 +0000

    Retirado função de select master no ficheiro de selects

--- funcoes_sqlite_select.py
+++ funcoes_sqlite_select.py
@@ -8,13 +8,6 @@ Este módulo contém funções que criam o código SQL para ser utilizado em *qu
 from ferramentas_BD import executarBD, get_tabelas
 import ferramentas_escolha
 
-def select_master():
-    query = f"""
-        SELECT name FROM sqlite_master WHERE type='table'
-    """
-
-    return query
-
 def select_geral(tabela):
     """
     Retorna código SQL para *queries select* de uma tabela.
