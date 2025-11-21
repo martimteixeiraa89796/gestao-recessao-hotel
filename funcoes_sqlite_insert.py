e722e95a0b644fcde054907123ce19e63c84c95b
Martim Teixeira <a89796@ualg.pt>
2025-11-21 21:12:00 +0000

    Retirado dados com auto incremento

--- funcoes_sqlite_insert.py
+++ funcoes_sqlite_insert.py
@@ -1,35 +1,38 @@
 from ferramentas_BD import executarBD
 
-def inserir_tipo_cama():
-    num_tipo_cama =int(input("insira tipo de cama:"))                 
+
+def inserir_tipo_cama():                 
     nome_tipo_cama=input("insira nome cama:")
   
     query = f"""
-        INSERT INTO Tipo_Cama (Num_Tipo_Cama, Nome_Tipo_Cama)
-        VALUES (?,?);
+        INSERT INTO Tipo_Cama (Nome_Tipo_Cama)
+        VALUES (?);
     """
-    executarBD(query, (num_tipo_cama, nome_tipo_cama))
-    
+    executarBD(query, (nome_tipo_cama))
+
+
 def inserir_tipo_Quarto():
-    Num_Tipo_Quarto =int(input("insira Numerotipocama:"))
     Nome_Tipo_Quarto =input("insira Tipo Quarto:")
     
     query = f"""
-    INSERT INTO Tipo_Quarto(Num_Tipo_Quarto,Nome_Tipo_Quarto)
-    VALUES (?,?);
+    INSERT INTO Tipo_Quarto(Nome_Tipo_Quarto)
+    VALUES (?);
 """
-    executarBD(query, (Num_Tipo_Quarto, Nome_Tipo_Quarto))
+    executarBD(query, (Nome_Tipo_Quarto))
+
 
 def inserir_quarto():
+    num_quarto = int("Insira numero do quarto")
     num_tipo_quarto = int(input("insira o numero do tipo de quarto"))
     preco = float(input(" o preco do quarto:"))
     ocupado = input("O quarto está ocupado (Sim/Não): ")
 
     query = f"""
-    INSERT INTO Quarto(Num_Tipo_Quarto,preco, ocupado)
-    VALUES (?,?,?);
+    INSERT INTO Quarto(Num_Quarto, Num_Tipo_Quarto, Preco, Ocupado)
+    VALUES (?, ? , ? , ?);
 """
-    executarBD(query, (num_tipo_quarto,preco, ocupado))
+    executarBD(query, (num_quarto, num_tipo_quarto, preco, ocupado))
+
 
 def inserir_cliente():
     nif = int(input("Insira o NIF do Cliente:"))
@@ -37,53 +40,53 @@ def inserir_cliente():
     telefone = input("Insira o telefone do Cliente:")
 
     query = f"""
-    INSERT INTO Cliente (NIF,Nome_Cliente,Telefone)
-    VALUES (?,?,?);
+    INSERT INTO Cliente (NIF, Nome_Cliente, Telefone)
+    VALUES (?, ?, ?);
     """
     executarBD(query, (nif,nome_cliente,telefone))
 
-def inserir_tipo_reserva():
 
-    num_tipo_reserva = int(input("Insira o numero  do tipo reservas:"))
+def inserir_tipo_reserva():
     nome_tipo_reserva = input("Insira o nome do tipo da reserva:")
 
     query = f"""
-    INSERT INTO Tipo_Reserva (Num_Tipo_Reserva, Nome_Tipo_Reserva)
-    VALUES (?,?);
+    INSERT INTO Tipo_Reserva (Nome_Tipo_Reserva)
+    VALUES (?);
     """
-    executarBD(query, (num_tipo_reserva,nome_tipo_reserva))
+    executarBD(query, (nome_tipo_reserva))
+
 
 def inserir_camas():
-
     num_quarto =int(input("Insira o numero do quarto:"))
     num_tipo_cama =int(input("Insira o numero do tipo de cama:"))
 
     query = f"""
     INSERT INTO tipo_Camas (Num_Quarto, Num_Tipo_Cama)
-    VALUES (?,?);
+    VALUES (?, ?);
     """
     executarBD(query, (num_quarto,num_tipo_cama))
 
+
 def inserir_funcao():
-    num_funcao = int(input("Insira o numero da funcao:"))
     nome_funcao = input("Insira o nome da funcao:")
 
     query = f"""
-    INSERT INTO funcoes (Num_Funcao, Nome_Funcao)
-    VALUES (?,?);
+    INSERT INTO funcoes (Nome_Funcao)
+    VALUES (?);
     """
-    executarBD(query, (num_funcao,nome_funcao))
+    executarBD(query, (nome_funcao))
+
 
 def inserir_Funcionario():
-    num_funcionario = int(input("Insira o número do funcionário: "))
     nome_funcionario = input("Insira o nome do funcionário: ")
     num_funcao = int(input("Insira o número da função: "))
 
     query = f"""
-    INSERT INTO Funcionario (Num_Funcionario, Nome_Funcionario, Num_Funcao)
-    VALUES (?, ?, ?);
+    INSERT INTO Funcionario (Nome_Funcionario, Num_Funcao)
+    VALUES (?, ?);
     """
-    executarBD(query, (num_funcionario,nome_funcionario,num_funcao))
+    executarBD(query, (nome_funcionario,num_funcao))
+
 
 def inserir_horario():
     num_funcionario = int(input("Insira o número do funcionário: "))
@@ -93,22 +96,23 @@ def inserir_horario():
 
     query = f"""
     INSERT INTO Horario (Num_Funcionario, Comeca, Acaba, Folga)
-    VALUES (?,?,?,?);
+    VALUES (?, ?, ?, ?);
     """
     executarBD(query, (num_funcionario,comeca,acaba,folga))
 
+
 def inserir_reserva():
-    num_reserva = int(input("Insira o número da reserva: "))
     num_tipo_reserva = int(input("Insira o número do tipo de reserva: "))
     check_in = input("Insira a data de check-in (AAAA-MM-DD HH:MM:SS): ")
     check_out = input("Insira a data de check-out (AAAA-MM-DD HH:MM:SS): ")
     num_funcionario = int(input("Insira o número do funcionário responsável: "))
 
     query = f"""
-    INSERT INTO Reserva (Num_Reserva, Num_Tipo_Reserva, Check_in, Check_out, Num_Funcionario)
-    VALUES (?,?,?,?,?);
+    INSERT INTO Reserva (Num_Tipo_Reserva, Check_in, Check_out, Num_Funcionario)
+    VALUES (?, ?, ?, ?, ?);
     """
-    executarBD(query, (num_reserva,num_tipo_reserva,check_in,check_out,num_funcionario))
+    executarBD(query, (num_tipo_reserva,check_in,check_out,num_funcionario))
+
 
 def inserir_hospede():
     num_reserva = int(input("Insira o número da reserva: "))
@@ -118,7 +122,7 @@ def inserir_hospede():
     
     query = f"""
     INSERT INTO Hospedes (Num_Reserva, NIF, Reservado_Em_Nome, Num_Quarto)
-    VALUES (?,?,?,?);
+    VALUES (?, ?, ?, ?);
     """
     executarBD(query, (num_reserva,nif,reservado_em_nome,num_quarto))
 
