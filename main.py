86f82a18cdcf4f5f659ec557003172df53ea0bf2
Martim Teixeira <a89796@ualg.pt>
2025-11-23 19:29:00 +0000

    Atualizada documentação

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ import funcoes_sqlite_update
 import verificar_e_atualizar_bd
 
 def continuar():
+    """
+    Mostra mensagem de confirmação.
+
+    Esta função simplesmente abranda a impressão do menu principal para que o utilizador poss ler o que fez.
+
+    Exemplo de execução:
+
+    >>> continuar()
+    #resultado
+    Primma Enter para continuar... 
+    """
+
     input("Primma Enter para continuar... ")
 
 
